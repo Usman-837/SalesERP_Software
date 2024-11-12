8a957143c93aa64b5e66d8f0baef3fe41c5c8a23
Muhammad Usman <usmantufail837@gmail.com>
2024-11-12 16:28:36 +0500

    add sale, customer receie, supplier payment, purchase button by new way using array of object and map method

--- src/components/Dashboard/Navbar.jsx
+++ src/components/Dashboard/Navbar.jsx
@@ -4,6 +4,30 @@ import { FaBalanceScale } from "react-icons/fa"
 import { FaRegMoneyBillAlt } from "react-icons/fa"
 
 export default function Navbar() {
+
+  const buttons = [
+    {
+      id: 1,
+      icon: <FaBalanceScale />,
+      text: "Sale"
+    },
+    {
+      id: 2,
+      icon: <FaRegMoneyBillAlt />,
+      text: "Customer Receive"
+    },
+    {
+      id: 3,
+      icon: <FaRegMoneyBillAlt />,
+      text: "Supplier Payment"
+    },
+    {
+      id: 4,
+      icon: <FaRegMoneyBillAlt />,
+      text: "Purchase"
+    }
+  ]
+
   return (
     <nav className='bg-white'>
       <div className='h-61  flex items-center'>
@@ -12,16 +36,37 @@ export default function Navbar() {
           <FaBars />
         </div>
         <div className='flex gap-x-2'>
-          {/* Sale Button */}
+          {
+            buttons.map((data) => {
+              return (
+                <div key={data.id} className='flex items-center gap-x-1 py-1.5 px-3 text-green text-sm border border-green hover:bg-green hover:text-white'>
+                  {data.icon}
+                  <a href=''>{data.text}</a>
+                </div>
+              )
+            })
+          }
+
+          {/* Sale Button
           <div className='flex items-center gap-x-1 py-1.5 px-3 text-green text-sm border border-green hover:bg-green hover:text-white'>
             <FaBalanceScale />
             <a href=''>Sale</a>
           </div>
-          {/* Customer Receive button */}
+          Customer Receive button
           <div className='flex items-center gap-x-1 py-1.5 px-3 text-green text-sm border border-green hover:bg-green hover:text-white'>
             <FaRegMoneyBillAlt />
             <a href=''>Customer Receive</a>
           </div>
+          Supplier Payment button
+          <div className='flex items-center gap-x-1 py-1.5 px-3 text-green text-sm border border-green hover:bg-green hover:text-white'>
+            <FaRegMoneyBillAlt />
+            <a href=''>Supplier Payment</a>
+          </div>
+          Purchase button
+          <div className='flex items-center gap-x-1 py-1.5 px-3 text-green text-sm border border-green hover:bg-green hover:text-white'>
+            <FaRegMoneyBillAlt />
+            <a href=''>Purchase</a>
+          </div> */}
         </div>
       </div>
     </nav>
