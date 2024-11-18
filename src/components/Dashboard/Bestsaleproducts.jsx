dba74cd12755332f5ddec74f863ee0263c421d64
Usman <usmantufail837@gmail.com>
2024-11-18 07:01:33 +0000

    add see all button and border button in best sale products

--- src/components/Dashboard/Bestsaleproducts.jsx
+++ src/components/Dashboard/Bestsaleproducts.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 const Bestsaleproducts = () => {
-  return (
-    <div>
-        <div>
-            <h4 className='text-lg text-[#374767]'>Best Sale Product</h4>
-        </div>
-    </div>
-  )
+    return (
+        <>
+            <hr className='my-5 text-slate-200' />
+            <div className='bg-white'>
+                <div className='py-2.5 px-fifteen flex justify-between border-b border-slate-300'>
+                    <h4 className='text-lg text-[#374767] pb-2.5'>Best Sale Product</h4>
+                    <a href='#' className='py-1.5 px-3 text-white bg-green rounded-sm'>See All</a>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Bestsaleproducts
