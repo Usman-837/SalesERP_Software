d2bda7f00291f228d6df8ac0ce4b802908a47464
Usman <usmantufail837@gmail.com>
2024-11-19 12:04:14 +0500

    add responsive functionality in overview box to make it responsive

--- src/components/Dashboard/Salessummary.jsx
+++ src/components/Dashboard/Salessummary.jsx
@@ -10,7 +10,7 @@ const Salessummary = () => {
                         <Summarygraph />
                     </div>
                 </div>
-                <div className='bg-white w-4/12'>
+                <div className='bg-white w-4/12 max-xl:w-full'>
                     <div className='py-2.5 p-fifteen border-b border-slate-300'>
                         <h4 className='text-lg text-[#374767] pb-2.5'>Today Overview</h4>
                     </div>
