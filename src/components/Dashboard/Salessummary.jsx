c2188b9fc2728efe565ffc6f2f9055db3ef5a0b7
Usman <usmantufail837@gmail.com>
2024-11-19 11:59:58 +0500

    add table in overview box

--- src/components/Dashboard/Salessummary.jsx
+++ src/components/Dashboard/Salessummary.jsx
@@ -14,8 +14,29 @@ const Salessummary = () => {
                     <div className='py-2.5 p-fifteen border-b border-slate-300'>
                         <h4 className='text-lg text-[#374767] pb-2.5'>Today Overview</h4>
                     </div>
-                    <div>
-                        
+                    <div className='p-fifteen'>
+                        <table className="text-sm border w-full text-left">
+                            <tbody>
+                                <tr className='bg-[#F9F9F9]'>
+                                    <th className='border border-slate-300 p-2 text-[#374767]'>Todays Report</th>
+                                    <th className='border border-slate-300 p-2 text-[#374767]'>TK</th>
+                                </tr>
+                                <tr>
+                                    <th className='border border-slate-300 p-2 text-[#374767]'>Total Sales</th>
+                                    <td className='border border-slate-300 p-2'>$ 0.00</td>
+                                </tr>
+                                <tr className='bg-[#F9F9F9]'>
+                                    <th className='border border-slate-300 p-2 text-[#374767]'>Total Purchase</th>
+                                    <td className='border border-slate-300 p-2'>$ 0.00</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table className="text-sm border w-full text-left mt-7">
+                            <tr className='bg-[#F9F9F9]'>
+                                <th className='border border-slate-300 p-2 text-[#374767]'>Last Sales</th>
+                                <th className='border border-slate-300 p-2 text-[#374767]'>TK</th>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
