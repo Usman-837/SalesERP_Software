70273891b4116b1fe670be10f1b0f23dd1d310d2
Usman <usmantufail837@gmail.com>
2024-11-20 10:38:51 +0500

    add sale menu with icon in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,7 @@ import profile from "../../assets/profile.jpg"
 import { TfiDashboard } from "react-icons/tfi"
 import { useSelector } from "react-redux"
 import { NavLink } from 'react-router-dom'
+import { FaBalanceScale } from "react-icons/fa"
 
 export default function Sidebar() {
   const isOpen = useSelector((state) => state.sidebar.isOpen);
@@ -30,10 +31,16 @@ export default function Sidebar() {
             </div>
           </div>
           {/* Sidebar Menu */}
-          <div className='flex items-center text-menucolor text-fifteen py-3 pr-five pl-fifteen hover:bg-[#3E454C] hover:text-white'>
+          <div className='flex items-center text-menucolor text-fifteen py-2.5 pr-five pl-fifteen hover:bg-[#3E454C] hover:text-white'>
             <div className='flex items-center'>
-              <TfiDashboard className='mr-2.5' />
+              <TfiDashboard className='mr-2.5 text-lg' />
               <NavLink to="/" end >Dashboard</NavLink>
+            </div>
+          </div>
+          <div className='flex items-center text-menucolor text-fifteen py-2.5 pr-five pl-fifteen hover:bg-[#3E454C] hover:text-white'>
+            <div className='flex items-center'>
+              <FaBalanceScale className='mr-2.5 text-lg'/>
+              <NavLink to="/sale" end >Sale</NavLink>
             </div>
           </div>
         </div>
