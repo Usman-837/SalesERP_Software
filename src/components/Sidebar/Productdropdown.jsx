f20c6dc67e3d67fb80b938660b3bfc1b4bb4b069
Usman <usmantufail837@gmail.com>
2024-11-21 10:39:05 +0500

    replace navlink tag for product title into span tag

--- src/components/Sidebar/Productdropdown.jsx
+++ src/components/Sidebar/Productdropdown.jsx
@@ -41,9 +41,7 @@ const Productdropdown = () => {
                         <summary className="flex items-center justify-between gap-2 p-2 font-medium marker:content-none cursor-pointer hover:bg-[#3E454C] hover:text-white pr-3 pl-fifteen group-open:bg-[#3E454C] group-open:text-white">
                             <div className="flex items-center">
                                 <FaBalanceScale className="mr-2.5 text-lg" />
-                                <NavLink to="/product" end>
-                                    Product
-                                </NavLink>
+                                <span>Product</span>
                             </div>
                             <MdKeyboardArrowLeft className="w-5 h-5 text-gray-500 transition-transform duration-500 ease-in-out group-open:rotate-[-90deg]" />
                         </summary>
